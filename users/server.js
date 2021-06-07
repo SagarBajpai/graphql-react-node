68acfe03d03970650c18fa6016e01ed538851087
SagarBajpai <ashversache@gmail.com>
2021-06-07 21:01:41 +0530

    feat: graphqlHTTP middleware added :fire:

--- users/server.js
+++ users/server.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const { graphqlHTTP } = require("express-graphql"); // This library is a glue-layer between express server and graphQL
 
 const app = express();
+
+// here we are setting up a middleware, that all the requests with endpoint 'graphql',
+// will be dealt by graphqlHTTP.
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    graphiql: true,
+  })
+);
 
 app.listen(4000, () => {
   console.log("Listening at 4000");
